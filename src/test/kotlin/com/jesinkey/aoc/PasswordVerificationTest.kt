62ecfc9e463ed3f578a1eb0f916ea73f0da75c20
Alexander Jesinkey <alexander.jesinkey@trioptima.com>
2019-12-04 16:02:27 +0100

    Tests for day 4 task 2

--- src/test/kotlin/com/jesinkey/aoc/PasswordVerificationTest.kt
+++ src/test/kotlin/com/jesinkey/aoc/PasswordVerificationTest.kt
@@ -28,4 +28,20 @@ internal class PasswordVerificationTest {
         val result = subject.verify("123789")
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `test 112233 is ok`() {
+        val subject = PasswordVerification()
+        val expected = true
+        val result = subject.verify("112233")
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `test 123444 is ok`() {
+        val subject = PasswordVerification()
+        val expected = false
+        val result = subject.verify("123444")
+        assertEquals(expected, result)
+    }
 }
