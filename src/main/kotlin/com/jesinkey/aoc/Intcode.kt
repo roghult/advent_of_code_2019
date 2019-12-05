787d8fa57bfe703b37109c5992c0b172f3790ac2
Alexander Jesinkey <alexander.jesinkey@gmail.com>
2019-12-05 21:12:52 +0100

    Refactor Intcode to better suit day 5

--- src/main/kotlin/com/jesinkey/aoc/Intcode.kt
+++ src/main/kotlin/com/jesinkey/aoc/Intcode.kt
@@ -1,28 +1,88 @@
 package com.jesinkey.aoc
 
-class Intcode {
+enum class ParameterMode(val code: Int) {
+    POSITION(0),
+    IMMEDIATE(1);
 
-    private val OPCODE = mapOf(
-        1 to {x: Int, y: Int -> x + y},
-        2 to {x: Int, y: Int -> x * y}
-    )
+    companion object {
+        private val values = values();
+        fun getByValue(value: Int) = values.first { it.code == value }
+    }
+}
+
+enum class Opcode(val code: Int) {
+    ADD(1),
+    MULTIPLY(2),
+    EXIT(99);
+
+    companion object {
+        private val values = Opcode.values();
+        fun getByValue(value: Int) = values.first { it.code == value }
+    }
+}
+
+class Intcode {
 
     fun run(sequence: List<Int>): List<Int> {
         val newSequence = sequence.toMutableList()
-        for (i in 0 until newSequence.size step 4) {
-            val opcode = newSequence[i]
-            if (opcode == 99) {
+        var pos = 0
+        while (pos != sequence.lastIndex) {
+            val instruction = newSequence[pos].toString()
+            val opcode = Opcode.getByValue(opcode(instruction))
+            if (opcode == Opcode.EXIT) {
                 return newSequence
             }
-            val posValue1 = newSequence[i + 1]
-            val posValue2 = newSequence[i + 2]
-            val outputPosition = newSequence[i + 3]
-            val value1 = newSequence[posValue1]
-            val value2 = newSequence[posValue2]
 
-            newSequence[outputPosition] = OPCODE.getValue(opcode)(value1, value2)
+            val parameterMode1 = parameterMode(instruction, 1)
+            val parameterMode2 = parameterMode(instruction, 2)
+            val outputParameterMode = parameterMode(instruction, 3)
+
+            pos += when (opcode) {
+                Opcode.ADD -> {
+                    val parameter1Value = value(newSequence, pos + 1, parameterMode1)
+                    val parameter2Value = value(newSequence, pos + 2, parameterMode2)
+                    val outputPosition = value(newSequence, pos + 3, outputParameterMode)
+                    newSequence[outputPosition] = parameter1Value + parameter2Value
+                    4
+                }
+                Opcode.MULTIPLY -> {
+                    val parameter1Value = value(newSequence, pos + 1, parameterMode1)
+                    val parameter2Value = value(newSequence, pos + 2, parameterMode2)
+                    val outputPosition = value(newSequence, pos + 3, outputParameterMode)
+                    newSequence[outputPosition] = parameter1Value * parameter2Value
+                    4
+                }
+                Opcode.EXIT -> return newSequence
+            }
         }
         return newSequence
+    }
+
+    private fun opcode(instruction: String): Int {
+        return instruction.takeLast(2).toInt()
+    }
+
+    private fun parameterMode(instruction: String, number: Int): ParameterMode {
+        val instructionPosition = number + 1
+        val parameterValue = when {
+            number == 3 -> {
+                1
+            }
+            instruction.length > instructionPosition -> {
+                instruction[instruction.length - instructionPosition].toInt()
+            }
+            else -> {
+                0
+            }
+        }
+        return ParameterMode.getByValue(parameterValue)
+    }
+
+    private fun value(sequence: List<Int>, position: Int, parameterMode: ParameterMode): Int {
+        return when (parameterMode) {
+            ParameterMode.POSITION -> sequence[sequence[position]]
+            ParameterMode.IMMEDIATE -> sequence[position]
+        }
     }
 }
 
