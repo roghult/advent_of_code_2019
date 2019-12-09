b707edda143c8887d1245e858279251ad5f9417f
Alexander Jesinkey <alexander.jesinkey@gmail.com>
2019-12-09 20:46:34 +0100

    Code improvements to Intcode

--- src/main/kotlin/com/jesinkey/aoc/Intcode.kt
+++ src/main/kotlin/com/jesinkey/aoc/Intcode.kt
@@ -1,7 +1,5 @@
 package com.jesinkey.aoc
 
-const val ZERO: Long = 0
-
 enum class ParameterMode(val code: Int) {
     POSITION(0),
     IMMEDIATE(1),
@@ -10,6 +8,7 @@ enum class ParameterMode(val code: Int) {
     companion object {
         private val values = values()
         fun getByValue(value: Int) = values.first { it.code == value }
+        fun getByValue(value: Char) = getByValue(Character.getNumericValue(value))
     }
 }
 
@@ -33,34 +32,30 @@ enum class Opcode(val code: Int) {
 
 class Intcode(val sequence: MutableMap<Long, Long>) {
 
+    private val OPCODE_LENGTH = 2
     private var instructionPointer: Long = 0
     private var relativeBase: Long = 0
 
     fun run(inputs: MutableList<Int> = mutableListOf()): Long? {
         while (true) { // ?
-            val instruction = sequence[instructionPointer].toString()
-            val opcode = Opcode.getByValue(opcode(instruction))
+            val opcode = Opcode.getByValue(opcode())
             if (opcode == Opcode.EXIT) {
                 return null // ?
             }
 
-            val parameterMode1 = parameterMode(instruction, 1)
-            val parameterMode2 = parameterMode(instruction, 2)
-            val parameterMode3 = parameterMode(instruction, 3)
-
             when (opcode) {
                 Opcode.ADD -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    val outputPosition = writeValue(instructionPointer + 3, parameterMode3)
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    val outputPosition = writeValue(3)
                     val newValue = parameter1Value + parameter2Value
                     setValue(outputPosition, newValue)
                     instructionPointer += 4
                 }
                 Opcode.MULTIPLY -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    val outputPosition = writeValue(instructionPointer + 3, parameterMode3)
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    val outputPosition = writeValue(3)
                     val newValue = parameter1Value * parameter2Value
                     setValue(outputPosition, newValue)
                     instructionPointer += 4
@@ -71,52 +66,52 @@ class Intcode(val sequence: MutableMap<Long, Long>) {
                     } else {
                         inputs.removeAt(0)
                     }
-                    val outputPosition = writeValue(instructionPointer + 1, parameterMode1)
+                    val outputPosition = writeValue(1)
                     setValue(outputPosition, input.toLong())
                     instructionPointer += 2
                 }
                 Opcode.OUTPUT -> {
-                    val output = readValue(instructionPointer + 1, parameterMode1)
+                    val output = readValue(1)
                     println(output)
                     instructionPointer += 2
                     return output
                 }
                 Opcode.JUMP_IF_TRUE -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    if (parameter1Value != ZERO) {
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    if (parameter1Value != 0L) {
                         instructionPointer = parameter2Value
                     } else {
                         instructionPointer += 3
                     }
                 }
                 Opcode.JUMP_IF_FALSE -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    if (parameter1Value == ZERO) {
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    if (parameter1Value == 0L) {
                         instructionPointer = parameter2Value
                     } else {
                         instructionPointer += 3
                     }
                 }
                 Opcode.LESS_THAN -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    val outputPosition = writeValue(instructionPointer + 3, parameterMode3)
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    val outputPosition = writeValue(3)
                     val value: Long = if (parameter1Value < parameter2Value) { 1 } else { 0 }
                     setValue(outputPosition, value)
                     instructionPointer += 4
                 }
                 Opcode.EQUALS -> {
-                    val parameter1Value = readValue(instructionPointer + 1, parameterMode1)
-                    val parameter2Value = readValue(instructionPointer + 2, parameterMode2)
-                    val outputPosition = writeValue(instructionPointer + 3, parameterMode3)
+                    val parameter1Value = readValue(1)
+                    val parameter2Value = readValue(2)
+                    val outputPosition = writeValue(3)
                     val value: Long = if (parameter1Value == parameter2Value) { 1 } else { 0 }
                     setValue(outputPosition, value)
                     instructionPointer += 4
                 }
                 Opcode.ADJUST_RELATIVE_BASE -> {
-                    val parameterValue1 = readValue(instructionPointer + 1, parameterMode1)
+                    val parameterValue1 = readValue(1)
                     relativeBase += parameterValue1
                     instructionPointer += 2
                 }
@@ -125,36 +120,41 @@ class Intcode(val sequence: MutableMap<Long, Long>) {
         }
     }
 
-    private fun opcode(instruction: String): Int {
-        return instruction.takeLast(2).toInt()
+    private fun opcode(): Int {
+        return currentInstruction().takeLast(OPCODE_LENGTH).toInt()
     }
 
-    private fun parameterMode(instruction: String, number: Int): ParameterMode {
-        val positionFromRight = number + 2
-        val positionFromLeft = instruction.length - positionFromRight
+    private fun parameterMode(number: Long): ParameterMode {
+        val instruction = currentInstruction()
+        val positionFromRight = number + OPCODE_LENGTH
+        val positionFromLeft = (instruction.length - positionFromRight).toInt()
         val parameterValue = when {
             positionFromLeft >= 0 -> {
-                instruction[positionFromLeft].toString().toInt()
+                instruction[positionFromLeft]
             }
             else -> {
-                0
+                '0'
             }
         }
         return ParameterMode.getByValue(parameterValue)
     }
 
-    private fun readValue(position: Long, parameterMode: ParameterMode): Long {
-        val positionValue = value(position)
-        return when (parameterMode) {
+    private fun currentInstruction(): String {
+        return sequence[instructionPointer].toString()
+    }
+
+    private fun readValue(parameterNumber: Long): Long {
+        val positionValue = value(instructionPointer + parameterNumber)
+        return when (parameterMode(parameterNumber)) {
             ParameterMode.POSITION -> value(positionValue)
             ParameterMode.IMMEDIATE -> positionValue
             ParameterMode.RELATIVE -> value(positionValue + relativeBase)
         }
     }
 
-    private fun writeValue(position: Long, parameterMode: ParameterMode): Long {
-        val positionValue = value(position)
-        return when (parameterMode) {
+    private fun writeValue(parameterNumber: Long): Long {
+        val positionValue = value(instructionPointer + parameterNumber)
+        return when (parameterMode(parameterNumber)) {
             ParameterMode.POSITION -> positionValue
             ParameterMode.IMMEDIATE -> positionValue
             ParameterMode.RELATIVE -> positionValue + relativeBase
@@ -168,35 +168,6 @@ class Intcode(val sequence: MutableMap<Long, Long>) {
     private fun setValue(position: Long, value: Long) {
         sequence[position] = value
     }
-
-//    private fun increaseSequence(to: Int) {
-//        for (x in sequence.size..to) {
-//            sequence.add(0)
-//        }
-//    }
-}
-
-data class NounAndVerb(
-    val noun: Int,
-    val verb: Int
-)
-
-fun nounAndVerbFinder(input: List<Int>, desiredOutput: Int, nouns: List<Int>, verbs: List<Int>): NounAndVerb? {
-    /*
-    val intcode = Intcode()
-    for (noun in nouns) {
-        for (verb in verbs) {
-            val input = INTCODE_INPUT.toMutableList()
-            input[1] = noun
-            input[2] = verb
-            val result = intcode.run(input)
-            if (result[0] == desiredOutput) {
-                return NounAndVerb(noun, verb)
-            }
-        }
-    }
-     */
-    return null
 }
 
 fun sequenceListToMap(sequence: List<Long>): MutableMap<Long, Long> {
